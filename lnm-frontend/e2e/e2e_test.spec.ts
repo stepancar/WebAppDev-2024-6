d5649fc630a04d859bc9dd616a3e08b316f8f6a5
KruglovEgor <367327@edu.itmo.ru>
2024-10-16 19:46:42 +0300

    comments
    Fixed comments

--- lnm-frontend/e2e/e2e_test.spec.ts
+++ lnm-frontend/e2e/e2e_test.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 test.describe('Example e2e Test Suite', () => {
 
     test('Home page loads with correct title', async ({ page }) => {
-        await page.goto('http://localhost:5173');  // URL вашего фронтенда
+        await page.goto('http://localhost:5173');
         await expect(page).toHaveTitle("Vite + React + TS");  // Проверка заголовка страницы
     });
 
     test('User can log in', async ({ page }) => {
-        await page.goto('http://localhost:5173');  // Путь к странице входа
+        await page.goto('http://localhost:5173');
         await page.click('input[type="checkbox"]')
         await page.fill('input[name="login"]', 'test_user');
         await page.fill('input[name="password"]', 'password123');
@@ -17,10 +17,10 @@ test.describe('Example e2e Test Suite', () => {
     });
 
     test('User can\'t log in', async ({ page }) => {
-        await page.goto('http://localhost:5173');  // Путь к странице входа
+        await page.goto('http://localhost:5173');
         await page.fill('input[name="login"]', 'test_user2');
         await page.fill('input[name="password"]', 'password1234');
         await page.click('button[type="submit"]');
-        await expect(page).toHaveURL('http://localhost:5173');
+        await expect(page).toHaveURL('http://localhost:5173'); // Проверка что остались там же
     });
 });
