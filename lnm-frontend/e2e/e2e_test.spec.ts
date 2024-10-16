8ba253d9c1df55c0a39d933e00173f1d33b91a47
KruglovEgor <367327@edu.itmo.ru>
2024-10-16 18:26:25 +0300

    restructure
    Renamed file. Also added new tests

--- lnm-frontend/e2e/e2e_test.spec.ts
+++ lnm-frontend/e2e/e2e_test.spec.ts
@@ -9,9 +9,18 @@ test.describe('Example e2e Test Suite', () => {
 
     test('User can log in', async ({ page }) => {
         await page.goto('http://localhost:5173');  // Путь к странице входа
+        await page.click('input[type="checkbox"]')
         await page.fill('input[name="login"]', 'test_user');
         await page.fill('input[name="password"]', 'password123');
         await page.click('button[type="submit"]');
-        // await expect(page).toHaveURL('http://localhost:3000/dashboard');  // Проверка перехода на dashboard
+        await expect(page).toHaveURL('http://localhost:5173/menu');  // Проверка перехода на menu
+    });
+
+    test('User can\'t log in', async ({ page }) => {
+        await page.goto('http://localhost:5173');  // Путь к странице входа
+        await page.fill('input[name="login"]', 'test_user2');
+        await page.fill('input[name="password"]', 'password1234');
+        await page.click('button[type="submit"]');
+        await expect(page).toHaveURL('http://localhost:5173');
     });
 });
