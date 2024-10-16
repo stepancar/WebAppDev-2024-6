599ef01121f4a4329d6ab3762f88ff984f0e73b9
KruglovEgor <367327@edu.itmo.ru>
2024-10-16 18:25:51 +0300

    updated
    Changed base URL and dir to the real one

--- lnm-frontend/e2e/playwright.config.ts
+++ lnm-frontend/e2e/playwright.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './e2e',
+    testDir: './',
     timeout: 60000,
     retries: 2,
     use: {
-        baseURL: 'http://localhost:3000', // базовый URL фронтенда
+        baseURL: 'http://localhost:5173', // базовый URL фронтенда
         headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
